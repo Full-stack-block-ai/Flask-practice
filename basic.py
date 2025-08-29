2bb80582685f65f4a665c4d82c402f80c0d0b2f9
Steve <stephenjmhough@outlook.com>
2025-08-29 06:02:23 +0100

    practice with Flaskform

---
+++ basic.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, session,redirect,url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField,BooleanField, DateTimeField,RadioField,SelectField,TextAreaField,SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secretkey'
+
+class InfoForm(FlaskForm):
+
+    breed = StringField("What breed are you", validators=[DataRequired()])
+    neutered = BooleanField('has the dog been neutered')
+    mood = RadioField('please choose mood:', choices=[('mood_one', 'Happy'), ('mood_two','Excited')])
+    food_choice = SelectField('pick your favourite food:', choices=[('chi','Chicken'), ('bf','beef')])
+    feedback = TextAreaField()
+    submit = SubmitField('submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+ 
+
+    form = InfoForm()
+
+    if form.validate_on_submit():
+        session['breed'] = form.breed.data
+        session['neutered'] = form.neutered.data
+        session['mood'] = form.mood.data
+        session['food_choice'] = form.food_choice.data
+        session['feedback'] = form.feedback.data
+        
+        return redirect(url_for('thankyou'))
+
+    return render_template('index.html', form=form)
+
+@app.route('/thankyou/')
+def thankyou():
+    return render_template('thankyou.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
